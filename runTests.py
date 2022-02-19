6df0b56aded01b9db6ff7bc45236b9eae0087060
Dargones <sasha.fedchin@gmail.com>
2022-02-19 16:29:09 -0500

    Fail workflow if a test fails

--- runTests.py
+++ runTests.py
@@ -1,17 +1,20 @@
 from logging import captureWarnings
 import subprocess
 from os import listdir
-
+import sys
 
 filename = "./toplevel.native"
 testsPath = "./tests/"
-args = "./tests/test0.roll"
 
 onlyfiles = [f for f in listdir(testsPath)]
+a_test_has_failed = False
 for f in onlyfiles:
     f = testsPath + f
     output = subprocess.run([filename, f], capture_output=True)
     if output.stderr != b'':
         print(f + ": Failed")
+        a_test_has_failed = True
     else:
         print(f + ": Passed")
+if (a_test_has_failed):
+    sys.exit(1)
